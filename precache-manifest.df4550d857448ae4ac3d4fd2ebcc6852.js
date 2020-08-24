e5de09c169f6c49703500e0dc45f0253a708cc00
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-24 10:57:53 +0530

    Updates

---
+++ precache-manifest.df4550d857448ae4ac3d4fd2ebcc6852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184a9562db2820f50ad64b460f8be103",
+    "url": "/react-router-practice/index.html"
+  },
+  {
+    "revision": "fa2453e0908a2366de86",
+    "url": "/react-router-practice/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "c17c8071f69ec46a97ae",
+    "url": "/react-router-practice/static/css/main.58538c93.chunk.css"
+  },
+  {
+    "revision": "fa2453e0908a2366de86",
+    "url": "/react-router-practice/static/js/2.1c275c87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router-practice/static/js/2.1c275c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17c8071f69ec46a97ae",
+    "url": "/react-router-practice/static/js/main.7569683f.chunk.js"
+  },
+  {
+    "revision": "84337a3f8f5a4cbf7d2a",
+    "url": "/react-router-practice/static/js/runtime-main.74bd17ad.js"
+  }
+]);
